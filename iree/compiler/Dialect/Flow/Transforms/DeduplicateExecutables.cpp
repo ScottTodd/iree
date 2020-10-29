a5d143457667494a0229ef7bbc6bf23687e73214
Scott Todd <scotttodd@google.com>
2020-10-29 14:54:47 -0700

    Hardcoded dedup for executables 0 and 1.

--- iree/compiler/Dialect/Flow/Transforms/DeduplicateExecutables.cpp
+++ iree/compiler/Dialect/Flow/Transforms/DeduplicateExecutables.cpp
@@ -21,10 +21,52 @@
 // TODO(scotttodd): pass statistics (number of executables deduped)
 // https://mlir.llvm.org/docs/PassManagement/#pass-statistics
 
+#include <iostream>  // DO NOT SUBMIT
+
 namespace mlir {
 namespace iree_compiler {
 namespace IREE {
 namespace Flow {
+
+namespace {
+
+// bool InterfaceOp::isEquivalentTo(InterfaceOp other) {
+//   auto bindings =
+//   llvm::to_vector<4>(getBlock().getOps<InterfaceBindingOp>()); auto
+//   otherBindings =
+//       llvm::to_vector<4>(other.getBlock().getOps<InterfaceBindingOp>());
+//   return bindings.size() == otherBindings.size() &&
+//          llvm::all_of(llvm::zip(bindings, otherBindings), [](auto bindings) {
+//            return OperationEquivalence::isEquivalentTo(std::get<0>(bindings),
+//                                                        std::get<1>(bindings));
+//          });
+// }
+
+bool areExecutablesEquivalent(ExecutableOp lhs, ExecutableOp rhs) {
+  // std::cerr << "LHS: " << std::endl;
+  // lhs.dump();
+  // std::cerr << "RHS: " << std::endl;
+  // rhs.dump();
+  auto lhsModule = lhs.getInnerModule();
+  auto rhsModule = rhs.getInnerModule();
+  return OperationEquivalence::isEquivalentTo(lhsModule, rhsModule);
+}
+
+// Replaces each usage of an entry point with its original symbol name with a
+// new symbol name.
+void replaceEntryPointUses(mlir::ModuleOp moduleOp,
+                           const DenseMap<Attribute, Attribute> &replacements) {
+  for (auto funcOp : moduleOp.getOps<mlir::FuncOp>()) {
+    funcOp.walk([&](DispatchOp dispatchOp) {
+      auto it = replacements.find(dispatchOp.entry_point());
+      if (it != replacements.end()) {
+        dispatchOp.entry_pointAttr(it->second.cast<SymbolRefAttr>());
+      }
+    });
+  }
+}
+
+}  // namespace
 
 class DeduplicateExecutablesPass
     : public PassWrapper<DeduplicateExecutablesPass, OperationPass<ModuleOp>> {
@@ -49,6 +91,51 @@ class DeduplicateExecutablesPass
 
     auto moduleOp = getOperation();
     auto builder = OpBuilder::atBlockBegin(moduleOp.getBody());
+
+    auto executableOps = llvm::to_vector<8>(moduleOp.getOps<ExecutableOp>());
+    if (executableOps.size() < 2) return;  // DO NOT SUBMIT
+
+    DenseMap<Attribute, Attribute> entryPointRefReplacements;
+
+    auto executable0 = executableOps[0];
+    auto executable1 = executableOps[1];
+    if (areExecutablesEquivalent(executable0, executable1)) {
+      std::cerr << "equivalent" << std::endl;
+
+      auto executable0EntryOp = executable0.getDispatchEntryOp();
+      auto executable1EntryOp = executable1.getDispatchEntryOp();
+      auto oldSymbolRefAttr = builder.getSymbolRefAttr(
+          executable1.getName(),
+          {builder.getSymbolRefAttr(executable1EntryOp.sym_name())});
+      auto newSymbolRefAttr = builder.getSymbolRefAttr(
+          executable0.getName(),
+          {builder.getSymbolRefAttr(executable0EntryOp.sym_name())});
+      entryPointRefReplacements[oldSymbolRefAttr] = newSymbolRefAttr;
+      // auto executable0EntryOps =
+      //     llvm::to_vector<8>(executable0.getOps<DispatchEntryOp>());
+      // auto executable1EntryOps =
+      //     llvm::to_vector<8>(executable1.getOps<DispatchEntryOp>());
+      // // TODO(scotttodd): error check
+      // for (int i = 0; i < executable0EntryOps.size(); ++i) {
+      //   auto oldSymbolRefAttr = builder.getSymbolRefAttr(
+      //       executable1.getName(),
+      //       {builder.getSymbolRefAttr(executable1EntryOps[i].sym_name())});
+      //   auto newSymbolRefAttr = builder.getSymbolRefAttr(
+      //       executable0.getName(),
+      //       {builder.getSymbolRefAttr(executable0EntryOps[i].sym_name())});
+      //   entryPointRefReplacements[oldSymbolRefAttr] = newSymbolRefAttr;
+      // }
+
+      replaceEntryPointUses(moduleOp, entryPointRefReplacements);
+
+      SymbolTable::replaceAllSymbolUses(executable1.sym_name(),
+                                        executable0.sym_name(),
+                                        &moduleOp.getBodyRegion());
+
+      executable1.erase();
+    } else {
+      std::cerr << "not equivalent" << std::endl;
+    }
   }
 };
 
