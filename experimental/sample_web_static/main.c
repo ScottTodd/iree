01ccad768368069a846f0f10f42019e4026ad9e6
Scott Todd <scotttodd@google.com>
2022-01-11 19:07:49 -0800

    Replace main with setup/cleanup/run. (breaks multithreaded sample further)

--- experimental/sample_web_static/main.c
+++ experimental/sample_web_static/main.c
@@ -10,8 +10,41 @@
 #include "iree/runtime/api.h"
 #include "iree/vm/bytecode_module.h"
 
+//===----------------------------------------------------------------------===//
+// Public API
+//===----------------------------------------------------------------------===//
+
+typedef struct iree_sample_state_t iree_sample_state_t;
+static void iree_sample_state_initialize(iree_sample_state_t* out_state);
+
+// TODO(scotttodd): figure out error handling and state management
+//     * out_state and return status would make sense, but emscripten...
+iree_sample_state_t* setup_sample();
+void cleanup_sample(iree_sample_state_t* state);
+
+void run_sample(iree_sample_state_t* state);
+
+//===----------------------------------------------------------------------===//
+// Implementation
+//===----------------------------------------------------------------------===//
+
 extern iree_status_t create_device_with_static_loader(
     iree_allocator_t host_allocator, iree_hal_device_t** out_device);
+
+typedef struct iree_sample_state_t {
+  iree_runtime_instance_t* instance;
+  iree_hal_device_t* device;
+  iree_runtime_session_t* session;
+  iree_vm_module_t* module;
+  iree_runtime_call_t call;
+} iree_sample_state_t;
+
+void iree_sample_state_initialize(iree_sample_state_t* out_state) {
+  out_state->instance = NULL;
+  out_state->device = NULL;
+  out_state->module = NULL;
+  out_state->session = NULL;
+}
 
 iree_status_t create_bytecode_module(iree_vm_module_t** out_module) {
   const struct iree_file_toc_t* module_file_toc =
@@ -22,19 +55,22 @@ iree_status_t create_bytecode_module(iree_vm_module_t** out_module) {
                                         iree_allocator_system(), out_module);
 }
 
-iree_status_t Run() {
+iree_sample_state_t* setup_sample() {
+  iree_sample_state_t* state;
+  state = malloc(sizeof(iree_sample_state_t));
+  iree_sample_state_initialize(state);
+
   iree_runtime_instance_options_t instance_options;
   iree_runtime_instance_options_initialize(IREE_API_VERSION_LATEST,
                                            &instance_options);
   // Note: no call to iree_runtime_instance_options_use_all_available_drivers().
-  iree_runtime_instance_t* instance = NULL;
 
   iree_status_t status = iree_runtime_instance_create(
-      &instance_options, iree_allocator_system(), &instance);
+      &instance_options, iree_allocator_system(), &state->instance);
 
-  iree_hal_device_t* device = NULL;
   if (iree_status_is_ok(status)) {
-    status = create_device_with_static_loader(iree_allocator_system(), &device);
+    status = create_device_with_static_loader(iree_allocator_system(),
+                                              &state->device);
   }
 
   iree_runtime_session_options_t session_options;
@@ -42,24 +78,50 @@ iree_status_t Run() {
   iree_runtime_session_t* session = NULL;
   if (iree_status_is_ok(status)) {
     status = iree_runtime_session_create_with_device(
-        instance, &session_options, device,
-        iree_runtime_instance_host_allocator(instance), &session);
+        state->instance, &session_options, state->device,
+        iree_runtime_instance_host_allocator(state->instance), &state->session);
   }
 
-  iree_vm_module_t* module = NULL;
   if (iree_status_is_ok(status)) {
-    status = create_bytecode_module(&module);
+    status = create_bytecode_module(&state->module);
   }
   if (iree_status_is_ok(status)) {
-    status = iree_runtime_session_append_module(session, module);
+    status = iree_runtime_session_append_module(state->session, state->module);
   }
 
   const char kMainFunctionName[] = "module.simple_mul";
-  iree_runtime_call_t call;
   if (iree_status_is_ok(status)) {
     status = iree_runtime_call_initialize_by_name(
-        session, iree_make_cstring_view(kMainFunctionName), &call);
+        state->session, iree_make_cstring_view(kMainFunctionName),
+        &state->call);
   }
+
+  if (!iree_status_is_ok(status)) {
+    iree_status_fprint(stderr, status);
+    iree_status_free(status);
+    free(state);
+    return NULL;
+  }
+
+  return state;
+}
+
+void cleanup_sample(iree_sample_state_t* state) {
+  iree_runtime_call_deinitialize(&state->call);
+
+  // Cleanup session and instance.
+  iree_hal_device_release(state->device);
+  iree_runtime_session_release(state->session);
+  iree_runtime_instance_release(state->instance);
+  iree_vm_module_release(state->module);
+
+  free(state);
+}
+
+void run_sample(iree_sample_state_t* state) {
+  iree_status_t status = iree_ok_status();
+
+  iree_runtime_call_reset(&state->call);
 
   // Populate initial values for 4 * 2 = 8.
   const int kElementCount = 4;
@@ -73,7 +135,7 @@ iree_status_t Run() {
       IREE_HAL_MEMORY_TYPE_HOST_LOCAL | IREE_HAL_MEMORY_TYPE_DEVICE_VISIBLE;
   if (iree_status_is_ok(status)) {
     status = iree_hal_buffer_view_allocate_buffer(
-        iree_hal_device_allocator(device), shape, IREE_ARRAYSIZE(shape),
+        iree_hal_device_allocator(state->device), shape, IREE_ARRAYSIZE(shape),
         IREE_HAL_ELEMENT_TYPE_FLOAT_32, IREE_HAL_ENCODING_TYPE_DENSE_ROW_MAJOR,
         input_memory_type,
         IREE_HAL_BUFFER_USAGE_DISPATCH | IREE_HAL_BUFFER_USAGE_TRANSFER,
@@ -83,7 +145,7 @@ iree_status_t Run() {
   }
   if (iree_status_is_ok(status)) {
     status = iree_hal_buffer_view_allocate_buffer(
-        iree_hal_device_allocator(device), shape, IREE_ARRAYSIZE(shape),
+        iree_hal_device_allocator(state->device), shape, IREE_ARRAYSIZE(shape),
         IREE_HAL_ELEMENT_TYPE_FLOAT_32, IREE_HAL_ENCODING_TYPE_DENSE_ROW_MAJOR,
         input_memory_type,
         IREE_HAL_BUFFER_USAGE_DISPATCH | IREE_HAL_BUFFER_USAGE_TRANSFER,
@@ -92,23 +154,23 @@ iree_status_t Run() {
         &arg1_buffer_view);
   }
   if (iree_status_is_ok(status)) {
-    status =
-        iree_runtime_call_inputs_push_back_buffer_view(&call, arg0_buffer_view);
+    status = iree_runtime_call_inputs_push_back_buffer_view(&state->call,
+                                                            arg0_buffer_view);
   }
   iree_hal_buffer_view_release(arg0_buffer_view);
   if (iree_status_is_ok(status)) {
-    status =
-        iree_runtime_call_inputs_push_back_buffer_view(&call, arg1_buffer_view);
+    status = iree_runtime_call_inputs_push_back_buffer_view(&state->call,
+                                                            arg1_buffer_view);
   }
   iree_hal_buffer_view_release(arg1_buffer_view);
 
   if (iree_status_is_ok(status)) {
-    status = iree_runtime_call_invoke(&call, /*flags=*/0);
+    status = iree_runtime_call_invoke(&state->call, /*flags=*/0);
   }
 
   iree_hal_buffer_view_t* ret_buffer_view = NULL;
   if (iree_status_is_ok(status)) {
-    status = iree_runtime_call_outputs_pop_front_buffer_view(&call,
+    status = iree_runtime_call_outputs_pop_front_buffer_view(&state->call,
                                                              &ret_buffer_view);
   }
 
@@ -129,34 +191,10 @@ iree_status_t Run() {
     }
   }
 
-  // Cleanup call and buffers.
-  iree_hal_buffer_view_release(ret_buffer_view);
-  iree_runtime_call_deinitialize(&call);
-
-  // Cleanup session and instance.
-  iree_hal_device_release(device);
-  iree_runtime_session_release(session);
-  iree_runtime_instance_release(instance);
-  iree_vm_module_release(module);
-
-  return status;
-}
-
-#include <math.h>
-int int_sqrt(int x) { return sqrt(x); }
-
-// TODO(scotttodd): more interesting demo:
-//    - [ ] MNIST or some other program instead of mul()
-//    - [ ] a few exported functions to call: init(), predict(), metadata()
-
-int main() {
-  const iree_status_t result = Run();
-  if (!iree_status_is_ok(result)) {
-    iree_status_fprint(stderr, result);
-    iree_status_free(result);
-    return -1;
+  if (!iree_status_is_ok(status)) {
+    iree_status_fprint(stderr, status);
+    iree_status_free(status);
+  } else {
+    fprintf(stdout, "run_sample completed successfully\n");
   }
-
-  fprintf(stdout, "static sample completed successfully\n");
-  return 0;
 }
