522c9e6f3c31d66630a2b47efbf5e161ee5eff24
Scott Todd <scotttodd@google.com>
2022-03-04 14:15:51 -0800

    Add CMake option for enabling coverage (untested).

--- build_tools/cmake/iree_copts.cmake
+++ build_tools/cmake/iree_copts.cmake
@@ -337,6 +337,18 @@ iree_select_compiler_opts(IREE_DEFAULT_LINKOPTS
     "-natvis:${CMAKE_SOURCE_DIR}/iree/iree.natvis"
 )
 
+# Code coverage.
+if(${IREE_ENABLE_COVERAGE})
+  iree_select_compiler_opts(IREE_DEFAULT_COPTS
+    CLANG_OR_GCC
+      "-coverage"
+  )
+  iree_select_compiler_opts(IREE_DEFAULT_LINKOPTS
+    CLANG_OR_GCC
+      "-coverage"
+  )
+endif()
+
 # Add to LINKOPTS on a binary to configure it for X/Wayland/Windows/etc
 # depending on the target cross-compilation platform.
 if(${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
