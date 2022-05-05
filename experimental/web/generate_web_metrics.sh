8134a560530b1c02809dd26e2c98505c6c33b4ea
Scott Todd <scotttodd@google.com>
2022-05-05 14:41:12 -0700

    Extend script to generate native artifacts and run benchmarks there.

--- experimental/web/generate_web_metrics.sh
+++ experimental/web/generate_web_metrics.sh
@@ -22,7 +22,8 @@
 # Sample usage:
 #   generate_web_metrics.sh /tmp/iree/web_metrics/
 #
-#   then look in that directory for any files you want to process further
+#   then look in that directory for any files you want to process further,
+#   and/or run run_native_benchmarks.sh to get native metrics for comparison
 
 set -e
 
@@ -31,6 +32,7 @@ if [ -z "$TARGET_DIR" ]; then
   echo "ERROR: Expected target directory (e.g. /tmp/iree/web_metrics/)"
   exit 1
 fi
+echo "Working in directory '$TARGET_DIR'"
 mkdir -p ${TARGET_DIR}
 cd ${TARGET_DIR}
 
@@ -42,9 +44,9 @@ cd ${TARGET_DIR}
 # Set up Python virtual environment                                           #
 ###############################################################################
 
-python -m venv .venv
-source .venv/bin/activate
-trap "deactivate 2> /dev/null" EXIT
+# python -m venv .venv
+# source .venv/bin/activate
+# trap "deactivate 2> /dev/null" EXIT
 
 # Install packages when you want by uncommenting this. Freezing to a specific
 # version when iterating on metrics is useful, and fetching is slow anyways.
@@ -57,48 +59,58 @@ trap "deactivate 2> /dev/null" EXIT
 # Download program source files                                               #
 ###############################################################################
 
+wget -nc https://storage.googleapis.com/iree-model-artifacts/mobile_ssd_v2_float_coco.tflite
+wget -nc https://storage.googleapis.com/iree-model-artifacts/deeplabv3.tflite
+wget -nc https://storage.googleapis.com/iree-model-artifacts/posenet.tflite
 wget -nc https://storage.googleapis.com/iree-model-artifacts/mobilebert-baseline-tf2-float.tflite
 wget -nc https://storage.googleapis.com/iree-model-artifacts/mobilenet_v2_1.0_224.tflite
+wget -nc https://storage.googleapis.com/iree-model-artifacts/MobileNetV3SmallStaticBatch.tflite
 
 ###############################################################################
 # Import programs into MLIR                                                   #
 ###############################################################################
 
+# Note: you can also download imported programs from runs of the
+# https://buildkite.com/iree/iree-benchmark-android pipeline.
+
 IREE_IMPORT_TFLITE_PATH=iree-import-tflite
 
 # import_program helper
 #   Args: program_name, tflite_source_path
-#   Imports tflite_source_path to program_name.mlir
+#   Imports tflite_source_path to program_name.tflite.mlir
 function import_program {
-  OUTPUT_FILE=./$1_tosa.mlir
-  echo "  Importing '$1' to '${OUTPUT_FILE}'..."
+  OUTPUT_FILE=./$1.tflite.mlir
+  echo "Importing '$1' to '${OUTPUT_FILE}'..."
   ${IREE_IMPORT_TFLITE_PATH?} $2 -o ${OUTPUT_FILE}
 }
 
-import_program "mobilebert_squad" "./mobilebert-baseline-tf2-float.tflite"
-import_program "mobilenet_v2" "./mobilenet_v2_1.0_224.tflite"
+import_program "deeplabv3" "deeplabv3.tflite"
+import_program "mobile_ssd_v2_float_coco" "mobile_ssd_v2_float_coco.tflite"
+import_program "posenet" "posenet.tflite"
+import_program "mobilebertsquad" "./mobilebert-baseline-tf2-float.tflite"
+import_program "mobilenet_v2_1.0_224" "./mobilenet_v2_1.0_224.tflite"
+import_program "MobileNetV3SmallStaticBatch" "MobileNetV3SmallStaticBatch.tflite"
 
 ###############################################################################
 # Compile programs                                                            #
 ###############################################################################
 
 # Either build from source (setting this path), or use from the python packages.
-IREE_COMPILE_PATH=~/code/iree-build/iree/tools/iree-compile
+# IREE_COMPILE_PATH=~/code/iree-build/iree/tools/iree-compile
+IREE_COMPILE_PATH="D:\dev\projects\iree-build\iree\tools\iree-compile"
 # IREE_COMPILE_PATH=iree-compile
 
-# compile_program helper
+# compile_program_wasm helper
 #   Args: program_name
-#   Compiles program_name_tosa.mlir to program_name_wasm.vmfb, dumping
+#   Compiles program_name.tflite.mlir to program_name_wasm.vmfb, dumping
 #   statistics and intermediate files to disk.
-function compile_program {
-  INPUT_FILE=./$1_tosa.mlir
+function compile_program_wasm {
+  INPUT_FILE=./$1.tflite.mlir
   OUTPUT_FILE=./$1_wasm.vmfb
-  echo "  Compiling '${INPUT_FILE}' to '${OUTPUT_FILE}'..."
+  echo "Compiling '${INPUT_FILE}' to '${OUTPUT_FILE}'..."
 
-  EXECUTABLES_DIR=./$1-executables/
-  STATISTICS_DIR=./$1-statistics/
-  mkdir -p ${EXECUTABLES_DIR}
-  mkdir -p ${STATISTICS_DIR}
+  ARTIFACTS_DIR=./$1-wasm_artifacts/
+  mkdir -p ${ARTIFACTS_DIR}
 
   # Compile from .mlir to .vmfb, dumping all the intermediate files and
   # compile-time statistics that we can.
@@ -106,10 +118,10 @@ function compile_program {
     --iree-input-type=tosa \
     --iree-hal-target-backends=llvm \
     --iree-llvm-target-triple=wasm32-unknown-emscripten \
-    --iree-hal-dump-executable-sources-to=${EXECUTABLES_DIR} \
-    --iree-hal-dump-executable-binaries-to=${EXECUTABLES_DIR} \
+    --iree-hal-dump-executable-sources-to=${ARTIFACTS_DIR} \
+    --iree-hal-dump-executable-binaries-to=${ARTIFACTS_DIR} \
     --iree-scheduling-dump-statistics-format=csv \
-    --iree-scheduling-dump-statistics-file=${STATISTICS_DIR}/$1_statistics.csv \
+    --iree-scheduling-dump-statistics-file=${ARTIFACTS_DIR}/$1_statistics.csv \
     --o ${OUTPUT_FILE}
 
   # Compress the .vmfb file (ideally it would be compressed already, but we can
@@ -117,8 +129,48 @@ function compile_program {
   gzip -k -f ${OUTPUT_FILE}
 }
 
-compile_program "mobilebert_squad"
-compile_program "mobilenet_v2"
+# compile_program_native helper
+#   Args: program_name
+#   Compiles program_name.tflite.mlir to program_name_native.vmfb, dumping
+#   statistics and intermediate files to disk.
+function compile_program_native {
+  INPUT_FILE=./$1.tflite.mlir
+  OUTPUT_FILE=./$1_native.vmfb
+  echo "Compiling '${INPUT_FILE}' to '${OUTPUT_FILE}'..."
+
+  ARTIFACTS_DIR=./$1-native_artifacts/
+  mkdir -p ${ARTIFACTS_DIR}
+
+  # Compile from .mlir to .vmfb, dumping all the intermediate files and
+  # compile-time statistics that we can.
+  ${IREE_COMPILE_PATH?} ${INPUT_FILE} \
+    --iree-input-type=tosa \
+    --iree-hal-target-backends=llvm \
+    --iree-hal-dump-executable-sources-to=${ARTIFACTS_DIR} \
+    --iree-hal-dump-executable-binaries-to=${ARTIFACTS_DIR} \
+    --iree-scheduling-dump-statistics-format=csv \
+    --iree-scheduling-dump-statistics-file=${ARTIFACTS_DIR}/$1_statistics.csv \
+    --o ${OUTPUT_FILE}
+
+  # Compress the .vmfb file (ideally it would be compressed already, but we can
+  # expect some compression support from platforms like the web).
+  gzip -k -f ${OUTPUT_FILE}
+}
+
+# compile_program helper
+#   Args: program_name
+#   Wraps compile_program_wasm and compile_program_native.
+function compile_program {
+  compile_program_wasm $1
+  compile_program_native $1
+}
+
+compile_program "deeplabv3"
+compile_program "mobile_ssd_v2_float_coco"
+compile_program "posenet"
+compile_program "mobilebertsquad"
+compile_program "mobilenet_v2_1.0_224"
+compile_program "MobileNetV3SmallStaticBatch"
 
 ###############################################################################
 # TODO: collect/summarize statistics (manual inspection or scripted)
