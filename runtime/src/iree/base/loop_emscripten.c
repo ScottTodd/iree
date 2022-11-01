a1a8c81f80343509db825cbbc8dd6ad94fa81b36
Scott Todd <scotttodd@google.com>
2022-11-01 15:38:15 -0700

    Progress implementing loop_emscripten.h/.c.

---
+++ runtime/src/iree/base/loop_emscripten.c
@@ -0,0 +1,355 @@
+// Copyright 2022 The IREE Authors
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+// #include "iree/base/internal/math.h"
+// #include "iree/base/internal/wait_handle.h"
+#include "iree/base/loop_emscripten.h"
+
+#include <emscripten.h>
+
+#include "iree/base/assert.h"
+
+//===----------------------------------------------------------------------===//
+// iree_loop_emscripten_t
+//===----------------------------------------------------------------------===//
+
+typedef struct iree_loop_emscripten_t {
+  iree_allocator_t allocator;
+
+  // TODO(scotttodd): other data
+
+  //   iree_loop_run_ring_t* run_ring;
+  //   iree_loop_wait_list_t* wait_list;
+
+  // Trailing data:
+  // + iree_loop_run_ring_storage_size
+  // + iree_loop_wait_list_storage_size
+} iree_loop_emscripten_t;
+
+IREE_API_EXPORT iree_status_t iree_loop_emscripten_allocate(
+    iree_allocator_t allocator, iree_loop_emscripten_t** out_loop_emscripten) {
+  IREE_ASSERT_ARGUMENT(out_loop_emscripten);
+
+  const iree_host_size_t loop_emscripten_size =
+      iree_host_align(sizeof(iree_loop_emscripten_t), iree_max_align_t);
+
+  uint8_t* storage = NULL;
+  IREE_RETURN_IF_ERROR(
+      iree_allocator_malloc(allocator, loop_emscripten_size, (void**)&storage));
+  iree_loop_emscripten_t* loop_emscripten = (iree_loop_emscripten_t*)storage;
+  loop_emscripten->allocator = allocator;
+
+  iree_status_t status = iree_ok_status();
+
+  if (iree_status_is_ok(status)) {
+    *out_loop_emscripten = loop_emscripten;
+  } else {
+    iree_loop_emscripten_free(loop_emscripten);
+  }
+  return status;
+}
+
+IREE_API_EXPORT void iree_loop_emscripten_free(
+    iree_loop_emscripten_t* loop_emscripten) {
+  IREE_ASSERT_ARGUMENT(loop_emscripten);
+  iree_allocator_t allocator = loop_emscripten->allocator;
+
+  // Abort all pending operations.
+  // This will issue callbacks for each operation that was aborted directly
+  // with IREE_STATUS_ABORTED.
+  // To ensure we don't enqueue more work while aborting we NULL out the lists.
+  //   iree_loop_run_ring_t* run_ring = loop_emscripten->run_ring;
+  //   iree_loop_wait_list_t* wait_list = loop_emscripten->wait_list;
+  //   loop_emscripten->run_ring = NULL;
+  //   loop_emscripten->wait_list = NULL;
+  //   iree_loop_wait_list_abort_all(wait_list);
+  //   iree_loop_run_ring_abort_all(run_ring);
+
+  // After all operations are cleared we can release the data structures.
+  //   iree_loop_run_ring_deinitialize(run_ring);
+  //   iree_loop_wait_list_deinitialize(wait_list);
+  iree_allocator_free(allocator, loop_emscripten);
+}
+
+static void iree_loop_emscripten_run_call(
+    iree_loop_emscripten_t* loop_emscripten, iree_loop_t loop,
+    const iree_loop_call_params_t params, iree_status_t op_status) {
+  iree_status_t status =
+      params.callback.fn(params.callback.user_data, loop, op_status);
+  if (!iree_status_is_ok(status)) {
+    // iree_loop_emscripten_emit_error(loop, status);
+  }
+}
+
+static void iree_loop_emscripten_run_dispatch(
+    iree_loop_emscripten_t* loop_emscripten, iree_loop_t loop,
+    const iree_loop_dispatch_params_t params) {
+  iree_status_t status = iree_ok_status();
+
+  // We run all workgroups before issuing the completion callback.
+  // If any workgroup fails we exit early and pass the failing status back to
+  // the completion handler exactly once.
+  uint32_t workgroup_count_x = params.workgroup_count_xyz[0];
+  uint32_t workgroup_count_y = params.workgroup_count_xyz[1];
+  uint32_t workgroup_count_z = params.workgroup_count_xyz[2];
+  iree_status_t workgroup_status = iree_ok_status();
+  for (uint32_t z = 0; z < workgroup_count_z; ++z) {
+    for (uint32_t y = 0; y < workgroup_count_y; ++y) {
+      for (uint32_t x = 0; x < workgroup_count_x; ++x) {
+        workgroup_status =
+            params.workgroup_fn(params.callback.user_data, loop, x, y, z);
+        if (!iree_status_is_ok(workgroup_status)) goto workgroup_failed;
+      }
+    }
+  }
+workgroup_failed:
+
+  // Fire the completion callback with either success or the first error hit by
+  // a workgroup.
+  status =
+      params.callback.fn(params.callback.user_data, loop, workgroup_status);
+  if (!iree_status_is_ok(status)) {
+    // iree_loop_emscripten_emit_error(loop, status);
+  }
+}
+
+// // Drains work from the loop until all work in |scope| has completed.
+// // A NULL |scope| indicates all work from all scopes should be drained.
+// static iree_status_t iree_loop_emscripten_drain_scope(
+//     iree_loop_emscripten_t* loop_emscripten,
+//     iree_loop_emscripten_scope_t* scope, iree_time_t deadline_ns) {
+//   do {
+//     // If we are draining a particular scope we can bail whenever there's no
+//     // more work remaining.
+//     if (scope && !scope->pending_count) break;
+
+//     // Run an op from the runnable queue.
+//     // We dequeue operations here so that re-entrant enqueuing works.
+//     // We only want to run one op at a time before checking our deadline so
+//     that
+//     // we don't get into infinite loops or exceed the deadline (too much).
+//     iree_loop_run_op_t run_op;
+//     if (iree_loop_run_ring_dequeue(loop_emscripten->run_ring, &run_op)) {
+//       iree_loop_t loop = {
+//           .self = run_op.scope,
+//           .ctl = iree_loop_emscripten_ctl,
+//       };
+//       switch (run_op.command) {
+//         case IREE_LOOP_COMMAND_CALL:
+//           iree_loop_emscripten_run_call(loop_emscripten, loop,
+//                                         run_op.params.call, run_op.status);
+//           break;
+//         case IREE_LOOP_COMMAND_DISPATCH:
+//           iree_loop_emscripten_run_dispatch(loop_emscripten, loop,
+//                                             run_op.params.dispatch);
+//           break;
+//       }
+//       continue;  // loop back around only if under the deadline
+//     }
+
+//     // -- if here then the run ring is currently empty --
+
+//     // If there are no pending waits then the drain has completed.
+//     if (iree_loop_wait_list_is_empty(loop_emscripten->wait_list)) {
+//       break;
+//     }
+
+//     // Scan the wait list and check for resolved ops.
+//     // If there are any waiting ops the next earliest timeout is returned. An
+//     // immediate timeout indicates that there's work in the run ring and we
+//     // shouldn't perform a wait operation this go around the loop.
+//     iree_time_t earliest_deadline_ns = IREE_TIME_INFINITE_FUTURE;
+//     IREE_RETURN_AND_END_ZONE_IF_ERROR(
+//         z0, iree_loop_wait_list_scan(loop_emscripten->wait_list,
+//                                      loop_emscripten->run_ring,
+//                                      &earliest_deadline_ns));
+//     if (earliest_deadline_ns != IREE_TIME_INFINITE_PAST &&
+//         earliest_deadline_ns != IREE_TIME_INFINITE_FUTURE) {
+//       // Commit the wait operation, waiting up until the minimum of the user
+//       // specified and wait list derived values.
+//       iree_time_t wait_deadline_ns = earliest_deadline_ns < deadline_ns
+//                                          ? earliest_deadline_ns
+//                                          : deadline_ns;
+//       IREE_RETURN_AND_END_ZONE_IF_ERROR(
+//           z0, iree_loop_wait_list_commit(loop_emscripten->wait_list,
+//                                          loop_emscripten->run_ring,
+//                                          wait_deadline_ns));
+//     }
+//   } while (iree_time_now() < deadline_ns);
+
+//   return iree_ok_status();
+// }
+
+IREE_API_EXPORT iree_status_t iree_loop_emscripten_wait_idle(
+    iree_loop_emscripten_t* loop_emscripten, iree_timeout_t timeout) {
+  IREE_ASSERT_ARGUMENT(loop_emscripten);
+  //   iree_time_t deadline_ns = iree_timeout_as_deadline_ns(timeout);
+  //   iree_status_t status = iree_loop_emscripten_drain_scope(
+  //       loop_emscripten, /*scope=*/NULL, deadline_ns);
+  //   return status;
+  return iree_ok_status();
+}
+
+// void test_function_pointer_from_js(int arg) {
+//   fprintf(stdout, "test_function_pointer_from_js, arg: %d\n", arg);
+// }
+
+// static iree_status_t iree_loop_emscripten_enqueue(
+//     iree_loop_emscripten_t* loop_emscripten, void* data) {
+//   return iree_ok_status();
+// }
+
+// EM_JS_DEPS(iree_loop_emscripten_ctl, "$dynCall");
+
+// Control function for the Emscripten loop.
+// DO NOT SUBMIT
+// ???? |self| must be an iree_loop_emscripten_scope_t ????.
+IREE_API_EXPORT iree_status_t
+iree_loop_emscripten_ctl(void* self, iree_loop_command_t command,
+                         const void* params, void** inout_ptr) {
+  IREE_ASSERT_ARGUMENT(self);
+
+  // iree_loop_emscripten_t* loop_emscripten = (iree_loop_emscripten_t*)self;
+
+  // TODO(scotttodd): try setImmediate()/setTimeout() here
+
+  // TODO(scotttodd): EM_JS to define a function, then call that function?
+  iree_loop_call_params_t* call_params = (iree_loop_call_params_t*)params;
+
+  // TODO(scotttodd): pass the entire command/params/etc. to JS?
+
+  // clang-format off
+  EM_ASM({
+    console.log('iree_loop_emscripten_ctl');
+    console.log('Module: ', Module);
+    // console.log('Module.Runtime.dynCall: ', Module.Runtime.dynCall);
+    // console.log('dynCall: ', dynCall);
+    console.log('arg0: ', $0);
+    // console.log('arg1: ', $1);
+
+    // Module.dynCall_viii()
+    Module['dynCall_vi']($0, 123);
+
+    setTimeout(() => {
+      //
+      console.log('iree_loop_emscripten_ctl -> setImmediate');
+
+      //
+      // iree_status_t status =
+      //     params.callback.fn(params.callback.user_data, loop, iree_ok_status());
+    }, 0);l
+  }, test_function_pointer_from_js);
+  // }, call_params->callback.fn, call_params->callback.user_data);
+  // clang-format on
+
+  // TODO(scotttodd): wrap args in void* struct?
+  //     allocator alloc, hop to js, call c, allocator free
+
+  // TODO(scotttodd): pass loop for reentrant scheduling
+  iree_status_t status = call_params->callback.fn(
+      call_params->callback.user_data, iree_loop_null(), iree_ok_status());
+  if (iree_status_is_ok(status)) {
+    fprintf(stdout, "callback fn was successful\n");
+  } else {
+    iree_status_fprint(stderr, status);
+    iree_status_free(status);
+  }
+
+  // Original C++:
+  //     callback.fn(callback.user_data);
+  // With setTimeout:
+  //     EM_ASM({
+  //       setTimeout(() => {
+  //         Module['dynCall_vi']($0, $1);
+  //       }, 0);
+  //     }, callback.fn, callback.user_data);
+
+  // https://stackoverflow.com/a/29319440
+
+  return iree_make_status(IREE_STATUS_UNIMPLEMENTED,
+                          "unimplemented loop command");
+
+  //   iree_loop_emscripten_scope_t* scope =
+  //       (iree_loop_emscripten_scope_t*)self;
+  //   iree_loop_emscripten_t* loop_emscripten = scope->loop_emscripten;
+
+  // if (IREE_UNLIKELY(!loop_emscripten->run_ring)) {
+  //   return iree_make_status(
+  //       IREE_STATUS_FAILED_PRECONDITION,
+  //       "new work cannot be enqueued while the loop is shutting down");
+  // }
+
+  // TODO(scotttodd): return immediately to make these all tail calls.
+  // switch (command) {
+  //   case IREE_LOOP_COMMAND_CALL:
+  //     return iree_loop_run_ring_enqueue(
+  //         loop_emscripten->run_ring,
+  //         (iree_loop_run_op_t){
+  //             .command = command,
+  //             .scope = scope,
+  //             .params =
+  //                 {
+  //                     .call = *(const iree_loop_call_params_t*)params,
+  //                 },
+  //         });
+  //   case IREE_LOOP_COMMAND_DISPATCH:
+  //     return iree_loop_run_ring_enqueue(
+  //         loop_emscripten->run_ring,
+  //         (iree_loop_run_op_t){
+  //             .command = command,
+  //             .scope = scope,
+  //             .params =
+  //                 {
+  //                     .dispatch = *(const
+  //                     iree_loop_dispatch_params_t*)params,
+  //                 },
+  //         });
+  //   case IREE_LOOP_COMMAND_WAIT_UNTIL:
+  //     return iree_loop_wait_list_insert(
+  //         loop_emscripten->wait_list,
+  //         (iree_loop_wait_op_t){
+  //             .command = command,
+  //             .scope = scope,
+  //             .params =
+  //                 {
+  //                     .wait_until =
+  //                         *(const iree_loop_wait_until_params_t*)params,
+  //                 },
+  //         });
+  //   case IREE_LOOP_COMMAND_WAIT_ONE:
+  //     return iree_loop_wait_list_insert(
+  //         loop_emscripten->wait_list,
+  //         (iree_loop_wait_op_t){
+  //             .command = command,
+  //             .scope = scope,
+  //             .params =
+  //                 {
+  //                     .wait_one = *(const
+  //                     iree_loop_wait_one_params_t*)params,
+  //                 },
+  //         });
+  //   case IREE_LOOP_COMMAND_WAIT_ALL:
+  //   case IREE_LOOP_COMMAND_WAIT_ANY:
+  //     return iree_loop_wait_list_insert(
+  //         loop_emscripten->wait_list,
+  //         (iree_loop_wait_op_t){
+  //             .command = command,
+  //             .scope = scope,
+  //             .params =
+  //                 {
+  //                     .wait_multi =
+  //                         *(const iree_loop_wait_multi_params_t*)params,
+  //                 },
+  //         });
+  //   case IREE_LOOP_COMMAND_DRAIN:
+  //     return iree_loop_emscripten_drain_scope(
+  //         loop_emscripten, scope,
+  //         ((const iree_loop_drain_params_t*)params)->deadline_ns);
+  //   default:
+  //     return iree_make_status(IREE_STATUS_UNIMPLEMENTED,
+  //                             "unimplemented loop command");
+  // }
+}
--- runtime/src/iree/base/loop_emscripten.h
+++ runtime/src/iree/base/loop_emscripten.h
@@ -7,7 +7,7 @@
 #ifndef IREE_BASE_LOOP_EMSCRIPTEN_H_
 #define IREE_BASE_LOOP_EMSCRIPTEN_H_
 
-#include <inttypes.h>
+// #include <inttypes.h>
 
 #include "iree/base/loop.h"
 
@@ -18,76 +18,37 @@ extern "C" {
 #endif  // __cplusplus
 
 //===----------------------------------------------------------------------===//
-// iree_loop_emscripten
+// iree_loop_emscripten_t
 //===----------------------------------------------------------------------===//
+
+// TODO(scotttodd): docs
+typedef struct iree_loop_emscripten_t iree_loop_emscripten_t;
+
+// Allocates a loop using |allocator| stored into |out_loop_emscripten|.
+IREE_API_EXPORT iree_status_t iree_loop_emscripten_allocate(
+    iree_allocator_t allocator, iree_loop_emscripten_t** out_loop_emscripten);
+
+// Frees |loop_emscripten|, aborting all pending operations.
+IREE_API_EXPORT void iree_loop_emscripten_free(
+    iree_loop_emscripten_t* loop_emscripten);
+
+// Initializes a loop.
+// TODO(scotttodd): pass javascript function pointer?
+// IREE_API_EXPORT void iree_loop_emscripten_initialize(
+//     iree_loop_emscripten_t* loop_emscripten);
+
+// TODO(scotttodd): struct for javascript function pointer and other info
 
 IREE_API_EXPORT iree_status_t
 iree_loop_emscripten_ctl(void* self, iree_loop_command_t command,
                          const void* params, void** inout_ptr);
-IREE_API_EXPORT iree_status_t
-iree_loop_emscripten_using_storage_ctl(void* self, iree_loop_command_t command,
-                                       const void* params, void** inout_ptr);
 
-// Returns a loop that doesn't really loop.
-// All operations are run as they are enqueued on the stack. This uses no
-// additional memory and ensures that everything completes upon return to the
-// user but does eliminate the ability for pipelining and overlapping work from
-// multiple subprograms. This approach limits the amount of work that can be
-// reentrantly scheduled and should only be used when in the tiniest of
-// environments with programs tested to be compatible with it.
-//
-// Reentrant enqueuing is possible and can be used to create tail call chains
-// (or recursion) that executes roughly in order.
-//
-// Caveats:
-// - Reentrant enqueuing of operations is limited to some small number (~4).
-// - Waits are performed as they are enqueued and the loop must be able to
-//   make forward progress on each.
-// - Execution deadlines are ignored in order to fully drain on each operation.
-// - Errors propagate immediately to the top-level caller and abort all pending
-//   operations.
-//
-// Thread-compatible: stateless and executes all work on the calling thread.
-static inline iree_loop_t iree_loop_emscripten(iree_status_t* out_status) {
-  iree_loop_t loop = {out_status, iree_loop_emscripten_ctl};
-  return loop;
-}
-
-// Minimum size in bytes required for iree_loop_emscripten_storage_t.
-// If we wanted to shrink this size to the absolute minimum we'd just expose the
-// structures here; not the worst thing but messy (as this is a public API).
-#define IREE_LOOP_EMSCRIPTEN_STORAGE_SIZE 512
-
-// Storage for an emscripten loop.
-// May be either allocated on the stack or on the heap and only needs to remain
-// valid for the lifetime of the iree_loop_t referencing it.
-typedef iree_alignas(iree_max_align_t) struct iree_loop_emscripten_storage_t {
-  uint8_t opaque[IREE_LOOP_EMSCRIPTEN_STORAGE_SIZE];
-  iree_status_t status;
-} iree_loop_emscripten_storage_t;
-
-// Returns an emscripten loop that uses an external |storage| instead of the
-// stack. The storage will only be used while executing and can be reused if the
-// caller knows it is safe (not reentrantly inside of a loop execution). Errors
-// that arise will be set in the storage status field and must be checked (or
-// ignored) by the caller to avoid leaks.
-//
-// See iree_loop_emscripten for details on the execution behavior.
-static inline iree_loop_t iree_loop_emscripten_initialize(
-    iree_loop_emscripten_storage_t* storage) {
-  storage->status = iree_ok_status();
+static inline iree_loop_t iree_loop_emscripten(iree_loop_emscripten_t* data) {
   iree_loop_t loop = {
-      storage,
-      iree_loop_emscripten_using_storage_ctl,
+      data,
+      iree_loop_emscripten_ctl,
   };
   return loop;
-}
-
-static void iree_loop_emscripten_deinitialize(
-    iree_loop_emscripten_storage_t* storage) {
-  if (!storage) return;
-  iree_status_ignore(storage->status);
-  storage->status = iree_ok_status();
 }
 
 #ifdef __cplusplus
