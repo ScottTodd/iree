4a2ee77cd9d4eae88ecd2fea6cfadecf92f2457a
Scott Todd <scott.todd0@gmail.com>
2024-02-19 13:00:00 -0800

    Convert I/O protobufs into npy files and try with tools.

--- experimental/regression_suite/scripts/run_onnx_tests.py
+++ experimental/regression_suite/scripts/run_onnx_tests.py
@@ -1,8 +1,21 @@
+# Copyright 2024 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+import onnx
 from pathlib import Path
-import os
+from onnx import numpy_helper
+import numpy as np
+
 
 GENERATED_TESTS_ROOT_DIR = "D:/dev/projects/onnx/onnx/backend/test/data/node"
+OUTPUT_ROOT_DIR = "D:/dev/projects/iree-tmp/2024_02_19_onnx"
 
+# iree-import-onnx D:\dev\projects\onnx\onnx\backend\test\data\node\test_abs\model.onnx -o D:\dev\projects\iree-tmp\2024_02_19_onnx\model_torch.mlir
+# iree-compile D:\dev\projects\iree-tmp\2024_02_19_onnx\model_torch.mlir --iree-hal-target-backends=llvm-cpu -o D:\dev\projects\iree-tmp\2024_02_19_onnx\model_cpu.vmfb
+# iree-run-module --module=D:\dev\projects\iree-tmp\2024_02_19_onnx\model_cpu.vmfb --device=local-task --input=@D:\dev\projects\iree-tmp\2024_02_19_onnx\input_0.npy --expected_output=@D:\dev\projects\iree-tmp\2024_02_19_onnx\output_0.npy
 
 def find_tests():
     # for root, dirs, files in os.walk(GENERATED_TESTS_ROOT_DIR):
@@ -20,6 +33,29 @@ def find_tests():
     return test_dirs
 
 
+def convert_io_proto(proto_filename, type_proto):
+    output_dir_path = Path(OUTPUT_ROOT_DIR)
+    output_file_path = output_dir_path / proto_filename.stem
+    print(f"Converting proto file '{proto_filename}' to '{output_file_path}'")
+
+    with open(proto_filename, "rb") as f:
+        protobuf_content = f.read()
+
+        if type_proto.HasField("tensor_type"):
+            tensor = onnx.TensorProto()
+            tensor.ParseFromString(protobuf_content)
+            t = numpy_helper.to_array(tensor)
+            # assert isinstance(t, np.ndarray)
+            # target_list.append(t)
+            print("  tensor_type -> TensorProto")
+            # print(f"   tensor: {t}")
+
+            np.save(output_file_path, t, allow_pickle=False)
+            print(f"    Saved to {output_file_path}")
+        else:
+            print(f"Unsupported proto type: {type_proto}")
+
+
 def run_test(test_dir):
     print(f"Running test for dir: '{test_dir.name}'")
 
@@ -35,13 +71,17 @@ def run_test(test_dir):
     inputs = list(test_data_dir.glob("input_*.pb"))
     outputs = list(test_data_dir.glob("output_*.pb"))
 
-    # TODO(scotttodd): handle > 1 input/output
-    input = inputs[0]
-    output = outputs[0]
-
     print(
         f"Testing model {model_path.name} with {inputs[0].name} and {outputs[0].name}"
     )
+
+    model = onnx.load(model_path)
+    convert_io_proto(inputs[0], model.graph.input[0].type)
+    convert_io_proto(outputs[0], model.graph.output[0].type)
+
+    # iree-import-onnx model.onnx -o model_torch.mlir
+    # iree-compile model_torch.mlir --iree-hal-target-backends=... -o model_*.vmfb
+    # input_*.pb and output_*.pb -> input.bin, output.bin
 
     # for test_data_dir in glob.glob(os.path.join(model_dir, "test_data_set*")):
     # inputs = []
